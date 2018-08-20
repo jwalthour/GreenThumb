b81921fc6b0de6a51162fccf081e26b47c39a206
John Walthour <john.walthour@gmail.com>
2018-08-20 07:10:59 -0400

    Updated to read two moisture sensors to a CSV output, power off in between, and read more intermittently.  For some reason, the two moisture sensors I bought are reading very differently.  ONe has a short-out value of about 200 ticks, the other about 1300 ticks.

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
+print("importing")
 import time
 import RPi.GPIO as GPIO
 import Adafruit_ADS1x15 # sudo pip install adafruit-ads1x15
-
+print("GPIO setup")
 GPIO.setmode(GPIO.BOARD)
 adc_pin = 7
 sens_pin = 13
 GPIO.setup([adc_pin,sens_pin], GPIO.OUT)
 
 # Power on sensor and ADC
+print("Connecting to ADC")
 GPIO.output([adc_pin,sens_pin],GPIO.HIGH)
 adc = Adafruit_ADS1x15.ADS1015(0x48)
+print("0,1")
 try:
 	while True:
-		print("Got sensor value: " + str(adc.read_adc(0)))
-		time.sleep(0.2)
+		print(str(adc.read_adc(0)) + "," + str(adc.read_adc(1)))
+		GPIO.output(sens_pin,GPIO.LOW)
+		time.sleep(30)
+		GPIO.output(sens_pin,GPIO.HIGH)
 finally:
 	print("Powering down sensor and ADC.")
 	GPIO.output([adc_pin,sens_pin],GPIO.LOW)
