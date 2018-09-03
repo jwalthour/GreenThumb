5845a186d139a7aa73eae5703489cfcc96b61d66
John Walthour <john.walthour@gmail.com>
2018-09-03 11:09:31 -0400

    Found the bug that was causing one sensor to read wrong

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ A system to automatically tend plants
 # Assembly notes
 - Cheap fountain pumps tend to have a limited "head" - the height to which they can raise water.  However, you absolutely must maintain a positive head - the outlet must be above the inlet - or else you will start a siphon.  If you have a negative head (reservoir water level is above the outlet), turning on the pump once will drain the whole reservoir into your plant.  The pump I bought was a simple centrifugal impeller, so when you turn it off, it does not impede the flow at all.
 - Wiring diagram: https://github.com/jwalthour/GreenThumb/blob/master/2018-8-26%20green%20thumb%20wiring%20diagram.pdf
+- Used this manual to set up the Pi as an access point: https://learn.sparkfun.com/tutorials/setting-up-a-raspberry-pi-3-as-an-access-point 
--- test.py
+++ test.py
@@ -2,26 +2,30 @@ print("importing")
 import time
 import RPi.GPIO as GPIO
 import Adafruit_ADS1x15 # sudo pip install adafruit-ads1x15
+
 print("GPIO setup")
 GPIO.setmode(GPIO.BOARD)
-adc_pin = 7
-sens_pin = 13
-GPIO.setup([adc_pin,sens_pin], GPIO.OUT)
+ADC_PIN_NUM = 7
+SENSE_PIN_0 = 13
+SENSE_PIN_1 = 29
+GPIO.setup([ADC_PIN_NUM,SENSE_PIN_0,SENSE_PIN_1], GPIO.OUT)
 
 # Power on sensor and ADC
 print("Connecting to ADC")
-GPIO.output([adc_pin,sens_pin],GPIO.HIGH)
+GPIO.output([ADC_PIN_NUM,SENSE_PIN_0,SENSE_PIN_1],GPIO.HIGH)
 adc = Adafruit_ADS1x15.ADS1015(0x48)
 print("0,1")
 try:
 	while True:
 		print(str(adc.read_adc(0)) + "," + str(adc.read_adc(1)))
-		GPIO.output(sens_pin,GPIO.LOW)
-		time.sleep(30)
-		GPIO.output(sens_pin,GPIO.HIGH)
+		GPIO.output(SENSE_PIN_0,GPIO.LOW)
+		GPIO.output(SENSE_PIN_1,GPIO.LOW)
+		time.sleep(1)
+		GPIO.output(SENSE_PIN_0,GPIO.HIGH)
+		GPIO.output(SENSE_PIN_1,GPIO.HIGH)
 finally:
 	print("Powering down sensor and ADC.")
-	GPIO.output([adc_pin,sens_pin],GPIO.LOW)
+	GPIO.output([ADC_PIN_NUM,SENSE_PIN_0,SENSE_PIN_1],GPIO.LOW)
 
 	# Explicitly do not cleanup.  This leaves the pins low, and thus the devices unpowered.
 	#GPIO.cleanup()
